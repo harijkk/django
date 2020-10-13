b31c700a23dc0bf346886ff4c19ac9359e16a305
Harikrishnan <harijkk@gmail.com>
2020-10-14 00:00:01 +0530

    UPDATED

--- xams/views.py
+++ xams/views.py
@@ -4,11 +4,11 @@ from .models import xam
 
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{'title':'EGOV PROJECT'})    
 
 def jee(request):
    
-    return render(request,'jee.html') 
+    return render(request,'jee.html',{'title':'APPLICATION'}) 
 def year(request):
     return render(request,'year.html')  
 def add1(request):
@@ -23,14 +23,14 @@ def add1(request):
    pic=request.FILES['pic']
    xaminfo=xam(name=name,place=place,adhar=adhar,gender=gender,religion=religion,bd=bd,mail=mail,pic=pic)
    xaminfo.save()
-   return render(request,'success.html') 
+   return render(request,'success.html',{'title':'SUBMITTED!!'}) 
 def home1(request):
     return redirect('home')   
 
 def about1(request):
-    return render(request,'about.html')    
+    return render(request,'about.html',{'title':'ABOUT US'})    
 def vit(request):
-    return render(request,'vit.html')    
+    return render(request,'vit.html',{'title':'VIT APPLICATION'})    
 
 
 # Create your views here.
